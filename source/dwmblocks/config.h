82ca44d49a998628244dbbb9006017c0c092f7ce
techno-sorcery <inquisitivecalculator@gmail.com>
2025-10-07 12:34:00 -0700

    Reconfigured dwmblocks

--- source/dwmblocks/config.h
+++ source/dwmblocks/config.h
@@ -4,10 +4,11 @@
 static const Block blocks[] = {
 	/*Delim?*/      /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{1,             "",	"tb-cpu",	        1,	    7},
-	{1,             "",	"tb-tmux",	        2,	    3},
+	// {1,             "",	"tb-tmux",	        2,	    3},
 	/* {1,             "",	"tb-thermal",	    2,	    3}, */
 	/* {1,             "",	"tb-memory",	    2,	    6}, */
 	{1,             "",	"tb-disk",	    2,	    3},
+	// {1,             "",	"tb-packages",	    5,	    11},
 	{1,             "",	"tb-network",	    5,	    11},
 	{1,             "",	"tb-power",	        5,	    4},
 	{1,             "",	"tb-brightness",	0,	    2},
